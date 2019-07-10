fecf448ef888a0aef417edb6fe145f9ac7170a5d
Zercon <zercon@list.ru>
2019-07-10 17:56:34 +0300

    added reading image from cam

--- samples/Main.cpp
+++ samples/Main.cpp
@@ -1,5 +1,35 @@
+#include <stdio.h>
+#include <iostream>
+
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+
+using namespace std;
+using namespace cv;
+
 int main()
 {
+	cout << "Enter SourceId (int): ";
+	int sourceId; // corresponds to what video canal we use for input
+	cin >> sourceId; 
 
+	cv::Mat frame;
+	cv::VideoCapture cam(sourceId);
+	while (waitKey(1) < 0)
+	{
+		cam >> frame;
+		if (frame.empty())
+			break;
+
+
+
+
+
+
+
+
+
+		imshow("hi", frame);
+	}
 	return 0;
 }
